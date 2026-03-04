ecdfe815b1b8bb518fd2b2aa0a4d3d73d9bd8c70
Sam Maton <sam.maton91@gmail.com>
2026-03-04 17:04:14 +0000

    setup basic bubble tea program

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"charm.land/bubbles/v2/textinput"
+	tea "charm.land/bubbletea/v2"
 )
 
+type model struct {
+	questions   []string
+	projectName string
+	auth        bool
+	input       textinput.Model
+}
+
+func NewModel(questions []string) model {
+	return model{
+		questions: questions,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) View() tea.View {
+	v := tea.NewView("Welcome to the project initializer!\n")
+	return v
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
 func main() {
-	fmt.Println("Hello world")
+	m := NewModel([]string{
+		"What is the name of your project?",
+		"Do you want to set up authentication? (yes/no)",
+	})
+	p := tea.NewProgram(m)
+
+	if _, err := p.Run(); err != nil {
+		panic(err)
+	}
 }
